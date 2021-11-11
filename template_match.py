8f83ea1cb78b7167dec574e52e9e66c07f7aee4d
yanjieze <zeyanjie@sjtu.edu.cn>
2021-11-11 23:40:15 +0800

    Finish template match. All algorithms are achieved. TODO: update the performance

--- template_match.py
+++ template_match.py
@@ -1,16 +1,20 @@
 import numpy as np
 import cv2
+from numpy.lib.shape_base import tile
 import utils
 import matplotlib.pyplot as plt
+import os
+import copy
+
 
 class TemplateMatcher:
-    def __init__(self, template_path='templates'):
+    def __init__(self, threshold=0.1, template_path='templates' ):
         self.template_path = template_path
         self.methods = ['cv2.TM_CCOEFF',  \
          'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
             'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
-        self.threshold = 0.90
-        self.visualize = True
+        self.threshold = threshold
+        self.visualize = False
 
     def match(self, img, template, method_idx=0):
         """
@@ -29,7 +33,7 @@ class TemplateMatcher:
         (tH, tW) = template.shape[:2]
 
         # loop over the scales of the image
-        for scale in np.linspace(0.1, 1.0, 20)[::-1]:
+        for scale in np.linspace(0.1, 1.0, 50)[::-1]:
             # resize the image according to the scale, and keep track
             # of the ratio of the resizing
             resized = cv2.resize(gray, ( int(gray.shape[1]*scale),int(gray.shape[0] * scale) ) )
@@ -42,7 +46,8 @@ class TemplateMatcher:
             # detect edges in the resized, grayscale image and apply template
             # matching to find the template in the image
             edged = cv2.Canny(resized, 50, 200)
-            result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
+            
+            result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF_NORMED)
             (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
             # check to see if the iteration should be visualized
             if visualize:
@@ -59,9 +64,14 @@ class TemplateMatcher:
 
         # unpack the bookkeeping variable and compute the (x, y) coordinates
         # of the bounding box based on the resized ratio
+        if found is None:
+            # print('match failure.')
+            return None
+            
         (maxVal, maxLoc, r) = found
         (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
         (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+
         # draw a bounding box around the detected result and display the image
         if visualize:
             cv2.rectangle(img, (startX, startY), (endX, endY), (0, 0, 255), 2)
@@ -69,23 +79,92 @@ class TemplateMatcher:
             cv2.waitKey(0)
 
 
-        print('score is:', maxVal)
-        if maxVal > self.threshold:
-            print('match success.')
-        else:
-            print('match failure.')
+        # print('score is:', maxVal)
+        # if maxVal > self.threshold:
+        #     print('match success.')
+        # else:
+        #     print('match failure.')
         
         return found
 
+
+    def template_dataset(self):
+        """
+        Return
+        """
+        self.template_file_list = os.listdir(self.template_path)
+        template_dataset = []
+        for template_file in self.template_file_list:
+            img = cv2.imread(os.path.join(self.template_path, template_file))
+            # parse file name
+            label = template_file.split('.')[0]
+            label = label.split('_')[0]
+            template_dataset.append( (label, img) )
+
+        return template_dataset
+
+
+    def loop_match(self, plate_img):
+
+        debug = True
+
+        template_dataset = self.template_dataset()
+        result_template_list = []
+        img = copy.deepcopy(plate_img)
+        # loop over
+        for data in template_dataset:
+            label = data[0]
+            template = data[1]
+            result = self.match(plate_img, template)
+            if result is None:
+                continue
+            else:
+                (tH, tW) = template.shape[:2]
+                (maxVal, maxLoc, r) = result
+                   
+                if maxVal > self.threshold:
+                    (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+                    (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+                    (midX, midY) = (int(startX/2+endX/2), int(startY/2+endY/2))
+                    if debug:
+                        cv2.rectangle(img, (startX, startY), (endX, endY), (0, 0, 255), 2)
+
+                    result_template_list.append((label, (startX,startY)))
+                    # result_template_list.append((label, (midX,midY)))
+                    # result_template_list.append((label, (endX,endY)))
+        
+        # filter
+        def compare(a):
+            return a[1][0]
+        result_template_list.sort(key=compare)
+
+        if debug:
+            utils.show_img(img)
+            print(result_template_list)
+        
+        output = []
+        for i in range(len(result_template_list)):
+            output.append(result_template_list[i][0])
+
+        output = "".join(output)
+
+        return output
+
+
+        
     
 if __name__=='__main__':
+
+    threshold = 0.01
+
     test_img_path = 'test1.jpg_plate.jpg' # 沪EWM957
-    test_template_path = 'templates/6.png'
+    test_template_path = 'templates/沪.png'
     
     test_img = cv2.imread(test_img_path)
     test_template = cv2.imread(test_template_path)
 
-    matcher = TemplateMatcher()
-    result = matcher.match(test_img, test_template)
-
+    matcher = TemplateMatcher(threshold=threshold)
+    # result = matcher.match(test_img, test_template)
+    plate_number = matcher.loop_match(test_img)
+    print(plate_number)
 
