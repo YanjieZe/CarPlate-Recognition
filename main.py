0ffc84a170ae4a8794704079d82323857d42aae7
yanjieze <zeyanjie@sjtu.edu.cn>
2021-11-17 15:44:57 +0800

    Finish NMS

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+from operator import pos
 import numpy as np
 import cv2
 import utils
 from localization import PlateLocation, draw_box
 from separation import get_area_img, postprocess
+from template_match import TemplateMatcher
 
 if __name__=='__main__':
     # for img_path,color in [("test2.jpg","green")]:
-    for img_path,color in [("test1.jpg", "blue"), ("test2.jpg","green"), ("test3.jpg","blue")]:
+    params =  [("test1.jpg", "blue", 0.1, 0.1), ("test2.jpg","green", 0.1, 0.1), ("test3.jpg","blue", 0.1, 0.1)]
+    for img_path,color,threshold,scale_speed in params:
         
         print('--------- test %s -------'%img_path)
         img = cv2.imread(img_path)
@@ -16,8 +19,9 @@ if __name__=='__main__':
         post_img = postprocess(sheared_img, color)
         
 
-        utils.save_img("%s_plate.jpg"%img_path, post_img)
-
-        # utils.show_img(post_img)
+        matcher = TemplateMatcher(threshold=threshold)
+        # result = matcher.match(test_img, test_template)
+        plate_number = matcher.loop_match(post_img, scale_speed)
+        print(plate_number)
 
 
--- template_match.py
+++ template_match.py
@@ -8,21 +8,22 @@ import copy
 
 
 class TemplateMatcher:
-    def __init__(self, threshold=0.1, template_path='templates' ):
+    def __init__(self, threshold=0.1, template_path='templates', visualize=False):
         self.template_path = template_path
         self.methods = ['cv2.TM_CCOEFF',  \
          'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
             'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
         self.threshold = threshold
-        self.visualize = False
+        self.visualize = visualize
 
-    def match(self, img, template, method_idx=0):
+    def match(self, img, template, scale_speed):
         """
         Multi scale Template match
         """
 
 
         template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+        
         template = cv2.Canny(template, 50, 200)
 
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -32,15 +33,26 @@ class TemplateMatcher:
 
         (tH, tW) = template.shape[:2]
 
+        # find scale
+        scale_low = 0.1
+        scale_high = 1
+        scale_num = 20
+        # import pdb; pdb.set_trace()
+        while(gray.shape[0]*scale_low < tH):
+            scale_low += scale_speed
+        scale_high = scale_low + 1.0
+
         # loop over the scales of the image
-        for scale in np.linspace(0.1, 1.0, 50)[::-1]:
+        for scale in np.linspace(scale_low, scale_high, scale_num)[::-1]:
             # resize the image according to the scale, and keep track
             # of the ratio of the resizing
             resized = cv2.resize(gray, ( int(gray.shape[1]*scale),int(gray.shape[0] * scale) ) )
+            # utils.show_img(resized)
             r = gray.shape[1] / float(resized.shape[1])
             # if the resized image is smaller than the template, then break
             # from the loop
             if resized.shape[0] < tH or resized.shape[1] < tW:
+                print('resize shape too small, break')
                 break
             
             # detect edges in the resized, grayscale image and apply template
@@ -65,7 +77,8 @@ class TemplateMatcher:
         # unpack the bookkeeping variable and compute the (x, y) coordinates
         # of the bounding box based on the resized ratio
         if found is None:
-            # print('match failure.')
+            if self.visualize:
+                print('Not found any. Match failure.')
             return None
             
         (maxVal, maxLoc, r) = found
@@ -73,38 +86,43 @@ class TemplateMatcher:
         (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
 
         # draw a bounding box around the detected result and display the image
-        if visualize:
+        if self.visualize:
             cv2.rectangle(img, (startX, startY), (endX, endY), (0, 0, 255), 2)
             cv2.imshow("Image", img)
             cv2.waitKey(0)
 
-
-        # print('score is:', maxVal)
-        # if maxVal > self.threshold:
-        #     print('match success.')
-        # else:
-        #     print('match failure.')
+        if self.visualize:
+            print('score is:', maxVal)
+            if maxVal > self.threshold:
+                print('match success.')
+            else:
+                print('match failure.')
         
         return found
 
 
     def template_dataset(self):
         """
-        Return
+        Return the templates dataset
         """
         self.template_file_list = os.listdir(self.template_path)
         template_dataset = []
+        
+
         for template_file in self.template_file_list:
+            if template_file=='.DS_Store':
+                continue
             img = cv2.imread(os.path.join(self.template_path, template_file))
             # parse file name
             label = template_file.split('.')[0]
             label = label.split('_')[0]
             template_dataset.append( (label, img) )
 
+        
         return template_dataset
 
 
-    def loop_match(self, plate_img):
+    def loop_match(self, plate_img, scale_speed):
 
         debug = True
 
@@ -115,7 +133,12 @@ class TemplateMatcher:
         for data in template_dataset:
             label = data[0]
             template = data[1]
-            result = self.match(plate_img, template)
+            try:
+                result = self.match(plate_img, template, scale_speed)
+            except:
+                import pdb; pdb.set_trace()
+                print('Error here.')
+
             if result is None:
                 continue
             else:
@@ -129,17 +152,19 @@ class TemplateMatcher:
                     if debug:
                         cv2.rectangle(img, (startX, startY), (endX, endY), (0, 0, 255), 2)
 
-                    result_template_list.append((label, (startX,startY)))
+                    result_template_list.append((label, (startX,startY, endX, endY)))
                     # result_template_list.append((label, (midX,midY)))
                     # result_template_list.append((label, (endX,endY)))
         
         # filter
         def compare(a):
             return a[1][0]
-        result_template_list.sort(key=compare)
+
+        result_template_list.sort(key=compare, reverse=False)
 
         if debug:
             utils.show_img(img)
+            utils.save_img('report_img/without_nms.png', img)
             print(result_template_list)
         
         output = []
@@ -157,14 +182,18 @@ if __name__=='__main__':
 
     threshold = 0.01
 
-    test_img_path = 'test1.jpg_plate.jpg' # 沪EWM957
-    test_template_path = 'templates/沪.png'
+    # test_img_path = 'test1.jpg_plate.jpg' # 沪EWM957
+    test_img_path = 'test2.jpg_plate.jpg' # 沪ADE6598
+    # test_img_path = 'test3.jpg_plate.jpg' # 皖SJ6M07
+
+    test_template_path = 'templates/D.png'
     
     test_img = cv2.imread(test_img_path)
     test_template = cv2.imread(test_template_path)
 
-    matcher = TemplateMatcher(threshold=threshold)
-    # result = matcher.match(test_img, test_template)
-    plate_number = matcher.loop_match(test_img)
-    print(plate_number)
+    matcher = TemplateMatcher(threshold=threshold, visualize=False)
+    # result = matcher.match(test_img, test_template, 0.1)
+
+    plate_number = matcher.loop_match(test_img, 0.3)
+    # print(plate_number)
 
